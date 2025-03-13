fa154a754fd4c6b12e39259e3befe8d60152a5c9
thisangaThewanmi <thisangat2005@gmail.com>
2025-03-13 14:11:11 +0530

    formatted the code structure

--- db/db.js
+++ db/db.js
@@ -5,21 +5,4 @@
  export var itemCart=[];
 
  // export var orderDetails=[];
- export let orderDetails = [
-  {
-   _orderId: 1,
-   _orderDate: "2023-10-01",
-   _customerName: "John Doe",
-   _total: 100.00,
-   _discount: 10.00,
-   _SubTotal: 90.00
-  },
-  {
-   _orderId: 2,
-   _orderDate: "2023-10-02",
-   _customerName: "Jane Smith",
-   _total: 200.00,
-   _discount: 20.00,
-   _SubTotal: 180.00
-  }
- ];
+ export let orderDetails = []
